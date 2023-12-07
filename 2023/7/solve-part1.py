f2f86d40e29f70b207b306c502fbb906d7c26488
Ekaitzo <antenastyle@gmail.com>
2023-12-07 13:06:16 +0100

    Day 7 Solution Uploaded

---
+++ 2023/7/solve-part1.py
@@ -0,0 +1,52 @@
+from collections import Counter
+
+file_path = '2023/7/input'
+
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+handsValue = {
+    (5,): 7,
+    (1,4): 6,
+    (2,3): 5,
+    (1,1,3): 4,
+    (1,2,2): 3,
+    (1,1,1,2): 2,
+    (1,1,1,1,1): 1
+}
+
+def getTotalWinnings(data):
+    hands, bids = [], []
+    for i in range(len(data)):
+        hands.append(data[i].split()[0])
+        bids.append(int(data[i].split()[1]))
+    handTypes = [(handsValue[tuple(sorted(Counter(hand).values()))], hand) for hand in hands]
+    handTypes.sort()
+    groupedHands = groupHands(handTypes)
+    ranks = rankHands(groupedHands, cards, data, hands)
+
+    totalWinnings = 0
+    for i in range(len(hands)):
+        totalWinnings += bids[i] * ranks[i]
+    return totalWinnings
+
+def groupHands(handTypes):
+    groupedType = {}
+    for handType in handTypes:
+        if handType[0] in groupedType:
+            groupedType[handType[0]].append(handType[1])
+        else:
+            groupedType[handType[0]] = [handType[1]]
+    return groupedType.values()
+
+def rankHands(groupedHands, cards, data, hands):
+    ranks = [0 for i in range(len(data))]
+    rank = 1
+    for group in groupedHands:
+        group = sorted(group, key=lambda s: tuple(cards.index(c) for c in s))
+        for hand in group:
+            ranks[hands.index(hand)] = rank
+            rank += 1
+    return ranks
+
+with open(file_path, 'r') as file:
+    data = [line for line in file.read().splitlines() if line]
+    print(getTotalWinnings(data))
